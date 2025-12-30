c09f7de20f59f623cfe38d64670bed33523be94a
Michael Forbes <90093092+michaelforbes-open@users.noreply.github.com>
2025-12-30 14:43:08 +0100

    Update run_demo.sh

--- security_demos/NXP_S32K/run_demo.sh
+++ security_demos/NXP_S32K/run_demo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "--- STEP 1: Compiling Firmware ---"
+echo "--- STEP 1: Compiling ---"
 arm-none-eabi-gcc -fno-stack-protector -z execstack -mcpu=cortex-m4 \
     -T firmware/linker.ld firmware/main.c -o firmware/vulnerable.elf -nostartfiles
 
 echo "--- STEP 2: Running Exploit ---"
-# renode-test handles the headless environment for us
-renode-test tests/exploit.robot
+# We use --show-log to see exactly why Renode might be failing internally
+renode-test --show-log tests/exploit.robot
