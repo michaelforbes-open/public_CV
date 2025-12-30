42773a3b9d8f099d86969f19bd1babfdca1b3c7f
Michael Forbes <90093092+michaelforbes-open@users.noreply.github.com>
2025-12-30 15:17:19 +0100

    Update run_demo.sh

--- security_demos/NXP_S32K/run_demo.sh
+++ security_demos/NXP_S32K/run_demo.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
+# Clean old files
+rm -f firmware/vulnerable.elf
 
-echo "--- STEP 1: Compiling Firmware ---"
+echo "--- STEP 1: Compiling ---"
 arm-none-eabi-gcc -fno-stack-protector -z execstack -mcpu=cortex-m4 \
     -T firmware/linker.ld firmware/main.c -o firmware/vulnerable.elf -nostartfiles
 
 echo "--- STEP 2: Running Exploit ---"
-# Use --headless to prevent any GUI-related silent crashes
 renode-test --headless tests/exploit.robot
